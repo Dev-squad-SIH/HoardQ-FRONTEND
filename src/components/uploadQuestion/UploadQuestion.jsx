a7f64f816bd5f2c1c9bf0b04926e25edf9ba057a
vyshnavi madikonda <vyshnavi.madikonda@gmail.com>
2022-03-15 01:08:52 +0530

    Made uploadquestion page responsive

--- src/components/uploadQuestion/UploadQuestion.jsx
+++ src/components/uploadQuestion/UploadQuestion.jsx
@@ -1,42 +1,105 @@
 import styles from './styles.module.css'
+import React, { useState } from 'react';
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button
+} from '@chakra-ui/react';
 
 
 const UploadQuestion = () => {
+  const [question, setQuestion] = useState('');
+  const [option, setOption] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [solution, setSolution] = useState('');
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(`Question: ${question} & Option: ${option} & Answer: ${answer} & Solution: ${solution}`);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.uploadquestion}>
-        <div className={styles.header}>
-          Upload Questions
-        </div>
-        <div className={styles.divider}></div>
+      <div className={styles.header}>Upload Question</div>
         <div className={styles.card}>
-        <form>
-          <div className={styles.contain} >
+        <form onSubmit={handleSubmit}>
+    {/* <Flex width="full" align="center" justifyContent="center">
+       <Box p={2}>
+        <Box textAlign="center">
+          <Heading>Upload Question</Heading>
+        </Box> */}
+        <div className={styles.contain} >
               <b className={styles.sub}>Subject</b>
+              <div className={styles.subjects}>
               <select name="languages" className={styles.subject}>
                 <option className={styles.inputElements} value="subject1">Subject1</option>
                 <option className={styles.inputElements} value="subject2">Subject2</option>
                 <option className={styles.inputElements} value="subject3">Subject3</option>
               </select>
+              </div>
               <b className={styles.typ}>Type</b>
+              <div className={styles.subjects}>
               <select name="languages" className={styles.type}>
                 <option className={styles.inputElements} value="mcq1">MCQs1</option>
                 <option className={styles.inputElements} value="mcq2">MCQ2</option>
                 <option className={styles.inputElements} value="mcq3">MCQ3</option>
               </select>
+              </div>
               <b className={styles.top}>Topic</b>
+              <div className={styles.subjects}>
               <select name="languages" className={styles.topic}>
                 <option className={styles.inputElements} value="topic1">Topic1</option>
                 <option className={styles.inputElements} value="topic2">Topic2</option>
                 <option className={styles.inputElements} value="topic3">Topic3</option>
               </select>
+              </div>
             </div>
+        {/* <Box my={4} textAlign="left">
+          <form>
+            <FormControl isRequired>
+              <FormLabel className={styles.question}>Question</FormLabel>
+              <Input className={styles.inputElementQuestion} type="question" placeholder="Enter Question" onChange={event => setQuestion(event.currentTarget.value)}/>
+            </FormControl>
+            <FormControl mt={6} isRequired>
+              <FormLabel className={styles.option}>Options</FormLabel>
+              <Input type="option"
+                className={styles.inputElement}
+                onChange={event => setOption(event.currentTarget.value)} />
+            </FormControl>
+            <div className={styles.add}>+</div>
+            <FormControl mt={6} isRequired>
+              <FormLabel className={styles.answer}>Answers</FormLabel>
+              <select name="languages" className={styles.answers} onChange={event => setAnswer(event.currentTarget.value)}>
+                <option className={styles.inputElements} value="1">1</option>
+                <option className={styles.inputElements} value="2">2</option>
+                <option className={styles.inputElements} value="3">3</option>
+                <option className={styles.inputElements} value="4">4</option>
+              </select>
+            </FormControl>
+            <FormControl mt={6} isRequired>
+              <FormLabel className={styles.solution}>Solution</FormLabel>
+              <input
+                type="text"
+                className={styles.inputElement}
+                placeholder="Enter Solution"
+                onChange={event => setSolution(event.currentTarget.value)}
+                required
+              />
+            </FormControl>
+          </form>
+        </Box>
+      </Box>
+    </Flex> */}
             <div className={styles.group} >
-              <b className={styles.question}>Question</b>
+              <b className={styles.question} required>Question</b>
               <input
                 type="text"
                 className={styles.inputElementQuestion}
                 placeholder="Enter Question"
+                onChange={event => setQuestion(event.currentTarget.value)}
                 required
               />
             </div>
@@ -45,12 +108,14 @@ const UploadQuestion = () => {
               <input
                 type="text"
                 className={styles.inputElement}
+                onChange={event => setOption(event.currentTarget.value)}
                 required
               />
             </div>
+            <div className={styles.add}>+</div>
             <div className={styles.group} >
               <b className={styles.answer}>Answer</b>
-              <select name="languages" className={styles.answers}>
+              <select name="languages" className={styles.answers} onChange={event => setAnswer(event.currentTarget.value)}>
                 <option className={styles.inputElements} value="1">1</option>
                 <option className={styles.inputElements} value="2">2</option>
                 <option className={styles.inputElements} value="3">3</option>
@@ -63,6 +128,7 @@ const UploadQuestion = () => {
                 type="text"
                 className={styles.inputElement}
                 placeholder="Enter Solution"
+                onChange={event => setSolution(event.currentTarget.value)}
                 required
               />
             </div>
--- src/components/uploadQuestion/index.js
+++ src/components/uploadQuestion/index.js
@@ -1,1 +1,12 @@
 export { UploadQuestion } from './UploadQuestion'
+// export const UploadQuestions = async ({ question, option, answer, solution }) => {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         if (question === 'question' && option === 'option' && answer === 'answer' && solution === 'solution') {
+//           resolve();
+//         } else {
+//           reject();
+//         }
+//       }, 3000);
+//     });
+//   };
