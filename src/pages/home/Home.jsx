cd486e0a2c041fa1928d3427df299cc8dc44226e
Shivam Sinha <steveneren4869@gmail.com>
2022-03-15 11:44:05 +0530

    Set-paper files added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.css'
 import Navbar from '../../components/navbar/index'
 import { VStack } from '@chakra-ui/react'
+import SetPaper from '../../components/setPaper/SetPaper'
 const Home = () => {
   return (
     <VStack>
-      <Navbar />
+      {/* <Navbar /> */}
+      <SetPaper/>
     </VStack>
   )
 }
